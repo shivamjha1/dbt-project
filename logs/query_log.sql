b79a18908acab7f0159b2e694e01ac8c68997eb8
Shivam Kumar Jha <shivamjha620@gmail.com>
2026-06-06 17:27:03 +0530

    first push-project ready

---
+++ logs/query_log.sql
@@ -0,0 +1,9 @@
+-- created_at: 2026-06-06T11:52:34.152377800+00:00
+-- finished_at: 2026-06-06T11:52:35.465238900+00:00
+-- elapsed: 1.3s
+-- outcome: success
+-- dialect: databricks
+-- node_id: not available
+-- query_id: 01f1619e-34b8-1f34-af14-d6db0cf73543
+-- desc: not provided
+select 1 as id;
